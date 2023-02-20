a80e9ae303a62ab2b94fad22753dd81faf6cf8f0
Devam Savjani <devamsavjani@rocketmail.com>
2023-02-20 14:19:37 +0000

    Add try catch finally bodies

--- code/historical-data/data_scraper.py
+++ code/historical-data/data_scraper.py
@@ -20,8 +20,6 @@ logging.basicConfig(filename='data_scraper.log',
                     datefmt='%H:%M:%S',
                     level=logging.DEBUG)
 
-logging.info("Running Urban Planning")
-
 logger = logging.getLogger('urbanGUI')
 
 # https://thegraph.com/docs/en/querying/graphql-api/
@@ -36,64 +34,63 @@ def get_block_data(pool_address, file_name):
 
     # write a row to the csv file
     writer.writerow(header)
-
-    result = gq_client.execute(
-        query="""
-            query ($id: ID!) {
-                pool(id: $id) {
-                    poolHourData(orderBy: periodStartUnix, first: 1000) {
-                        id
-                        token0Price
-                        token1Price
-                        periodStartUnix
-                        liquidity
-                        feesUSD
-                    }
-                }
-            }
-        """,
-        operation_name='foo',
-        variables={"id": pool_address})
-
-    hourlyData = json.loads(result)['data']['pool']['poolHourData']
-
-    while len(hourlyData) > 0:
-        rows = [[hourData[key] for key in header] for hourData in hourlyData]
-        writer.writerows(rows)
-        # print(hourlyData)
-
-        prev_max_time = hourlyData[-1]['periodStartUnix']
-        # print(prev_max_time)
-
+    
+    try:
         result = gq_client.execute(
             query="""
-                query ($id: ID!, $prev_max_time: Int!) {
+                query ($id: ID!) {
                     pool(id: $id) {
-                        poolHourData(
-                            where: {periodStartUnix_gt: $prev_max_time}, orderBy: periodStartUnix, first: 1000
-                            ){
-                                id
-                                token0Price
-                                token1Price
-                                periodStartUnix
-                                liquidity
-                                feesUSD
-                            }
+                        poolHourData(orderBy: periodStartUnix, first: 1000) {
+                            id
+                            token0Price
+                            token1Price
+                            periodStartUnix
+                            liquidity
+                            feesUSD
+                        }
                     }
                 }
             """,
             operation_name='foo',
-            variables={"id": pool_address, "prev_max_time": prev_max_time})
+            variables={"id": pool_address})
 
-        # print(result)
-        if json.loads(result)['data']:
+        hourlyData = json.loads(result)['data']['pool']['poolHourData']
+        rows = [[hourData[key] for key in header] for hourData in hourlyData]
+        writer.writerows(rows)
+        
+        while len(hourlyData) >= 1000:
+            prev_max_time = hourlyData[-1]['periodStartUnix']
+
+            result = gq_client.execute(
+                query="""
+                    query ($id: ID!, $prev_max_time: Int!) {
+                        pool(id: $id) {
+                            poolHourData(
+                                where: {periodStartUnix_gt: $prev_max_time}, orderBy: periodStartUnix, first: 1000
+                                ){
+                                    id
+                                    token0Price
+                                    token1Price
+                                    periodStartUnix
+                                    liquidity
+                                    feesUSD
+                                }
+                        }
+                    }
+                """,
+                operation_name='foo',
+                variables={"id": pool_address, "prev_max_time": prev_max_time})
+
             hourlyData = json.loads(result)['data']['pool']['poolHourData']
-        else:
-            logging.error('Error fetching in ' + file_name)
-            hourlyData = []
+            rows = [[hourData[key] for key in header] for hourData in hourlyData]
+            writer.writerows(rows)
 
-    # close the file
-    f.close()
+    except:
+        print('ERROR: ' +  file_name)
+        logger.error('Failed to fetch some data for address ' + pool_address + ' : ' +  file_name)
+    finally:
+        # close the file
+        f.close()
 
 def generate_file_path(token0, token1):
     basepath = f"liquidity_pool_data/{token0}_{token1}_pricing_data"
