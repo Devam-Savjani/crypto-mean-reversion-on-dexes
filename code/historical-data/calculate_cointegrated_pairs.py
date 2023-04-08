e9b7e29646faadb5d6a40b42d2171e41fd986a32
Devam Savjani <devamsavjani@rocketmail.com>
2023-04-08 19:17:45 +0100

    Use database to calculate cointegrated pairs

--- code/historical-data/calculate_cointegrated_pairs.py
+++ code/historical-data/calculate_cointegrated_pairs.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from check_liquidity_pool_data import get_pools_with_data_till_today
+from database_interactions import table_to_df
+from check_liquidity_pool_data import get_pools_max_timestamp
 from tqdm import tqdm
 import pickle
 import statsmodels.api as sm
@@ -10,21 +11,20 @@ liquidity_pool_data_directory = 'liquidity_pool_data'
 
 def calculate_pairs_sum_of_squared_differences():
     liquidity_pool_pair_ssds = {}
-    valid_liquidity_pools = get_pools_with_data_till_today()
+    valid_liquidity_pools = get_pools_max_timestamp()['table_name']
 
     for i in tqdm(range(len(valid_liquidity_pools))):
         for j in range(i+1, len(valid_liquidity_pools)):
+    
+            merged = table_to_df(command=f"""
+                SELECT p1.period_start_unix as period_start_unix, p1.id as p1_id, p1.token1_price_ratio as p1_token1_price_ratio, p2.id as p2_id, p2.token1_price_ratio as p2_token1_price_ratio
+                FROM "{valid_liquidity_pools[i]}" as p1 INNER JOIN "{valid_liquidity_pools[j]}" as p2
+                ON p1.period_start_unix = p2.period_start_unix;
+                """)
 
-            datapair1 = pd.read_csv(valid_liquidity_pools[i])
-            datapair1 = datapair1.drop(['liquidity', 'feesUSD'], axis=1)
+            ssd = np.sum((merged['p1_token1_price_ratio'].to_numpy() - merged['p2_token1_price_ratio'].to_numpy())**2)
 
-            datapair2 = pd.read_csv(valid_liquidity_pools[j])
-            datapair2 = datapair2.drop(['liquidity', 'feesUSD'], axis=1)
-
-            merged = pd.merge(datapair1, datapair2, on='periodStartUnix')
-            ssd = np.sum((merged['token1PriceRatio_x'].to_numpy() - merged['token1PriceRatio_y'].to_numpy())**2)
-
-            key = valid_liquidity_pools[i].split('/')[1][:-4] + '/' + valid_liquidity_pools[j].split('/')[1][:-4]
+            key = (valid_liquidity_pools[i], valid_liquidity_pools[j])
             liquidity_pool_pair_ssds[key] = ssd
 
     liquidity_pool_pair_ssds = sorted(liquidity_pool_pair_ssds.items(), key=lambda x:x[1])
@@ -41,18 +41,16 @@ def load_pairs_sum_of_squared_differences():
 
     return liquidity_pool_pair_ssds
 
-def is_cointegrated(liquidity_pair):
-    pair = liquidity_pair.split('/')
-    pair0 = pair[0]
-    pair1 = pair[1]
+def is_cointegrated(p1, p2):
 
-    pair0_df = pd.read_csv(f"{liquidity_pool_data_directory}/{pair0}.csv")
-    pair1_df = pd.read_csv(f"{liquidity_pool_data_directory}/{pair1}.csv")
+    merged = table_to_df(command=f"""
+                SELECT p1.period_start_unix as period_start_unix, p1.id as p1_id, p1.token1_price_ratio as p1_token1_price_ratio, p2.id as p2_id, p2.token1_price_ratio as p2_token1_price_ratio
+                FROM "{p1}" as p1 INNER JOIN "{p2}" as p2
+                ON p1.period_start_unix = p2.period_start_unix;
+                """)
     
-    merged = pd.merge(pair0_df, pair1_df, on='periodStartUnix')
-
-    df1 = merged['token1PriceRatio_x']
-    df2 = merged['token1PriceRatio_y']
+    df1 = merged['p1_token1_price_ratio']
+    df2 = merged['p2_token1_price_ratio']
 
     # Step 1: Test for unit roots
     adf1 = adfuller(df1)
@@ -81,9 +79,8 @@ def is_cointegrated(liquidity_pair):
 def get_top_n_cointegrated_pairs(ssds, n):
     cointegrated_pairs = []
     for pair in tqdm(ssds):
-        liquidity_pair_str, _ = pair
-        if is_cointegrated(liquidity_pair_str):
-            print('Found One')
+        p1, p2 = pair[0]
+        if is_cointegrated(p1, p2):
             cointegrated_pairs.append(pair)
             if len(cointegrated_pairs) == n:
                 return cointegrated_pairs
@@ -92,6 +89,6 @@ def get_top_n_cointegrated_pairs(ssds, n):
 
 # calculate_pairs_sum_of_squared_differences()
 ssds = load_pairs_sum_of_squared_differences()
-foo = get_top_n_cointegrated_pairs(ssds, 2)
+foo = get_top_n_cointegrated_pairs(ssds, 20)
 print(*foo, sep="\n")
 
