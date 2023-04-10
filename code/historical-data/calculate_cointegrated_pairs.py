a4d405aa52fa177791c20212ec6e4e3bd17feb9c
Devam Savjani <devamsavjani@rocketmail.com>
2023-04-10 14:32:51 +0100

    Split saving and loading of ssds and cointegrated pairs

--- code/historical-data/calculate_cointegrated_pairs.py
+++ code/historical-data/calculate_cointegrated_pairs.py
@@ -29,19 +29,37 @@ def calculate_pairs_sum_of_squared_differences():
 
     liquidity_pool_pair_ssds = sorted(liquidity_pool_pair_ssds.items(), key=lambda x:x[1])
 
-    with open('cointegrated_pairs_ssd.pickle', 'wb') as f:
-        pickle.dump(liquidity_pool_pair_ssds, f)
+    return save_pairs_sum_of_squared_differences(liquidity_pool_pair_ssds)
+
+def save_pairs_sum_of_squared_differences(ssds):
+    with open('sum_square_differences.pickle', 'wb') as f:
+        pickle.dump(ssds, f)
 
     f.close()
+    return ssds
 
 def load_pairs_sum_of_squared_differences():
-    with open('cointegrated_pairs_ssd.pickle', 'rb') as f:
+    with open('sum_square_differences.pickle', 'rb') as f:
         liquidity_pool_pair_ssds = pickle.load(f)
         f.close()
 
     return liquidity_pool_pair_ssds
 
-def is_cointegrated(p1, p2):
+def save_cointegrated_pairs(cointegrated_pairs):
+    with open('cointegrated_pairs.pickle', 'wb') as f:
+        pickle.dump(cointegrated_pairs, f)
+
+    f.close()
+    return cointegrated_pairs
+
+def load_cointegrated_pairs():
+    with open('cointegrated_pairs.pickle', 'rb') as f:
+        cointegrated_pairs = pickle.load(f)
+        f.close()
+
+    return cointegrated_pairs
+
+def get_is_cointegrated_and_hedge_ratio(p1, p2):
 
     merged = table_to_df(command=f"""
                 SELECT p1.period_start_unix as period_start_unix, p1.id as p1_id, p1.token1_price_ratio as p1_token1_price_ratio, p2.id as p2_id, p2.token1_price_ratio as p2_token1_price_ratio
@@ -58,7 +76,7 @@ def is_cointegrated(p1, p2):
 
     if adf1[0] < adf1[4]['5%'] and adf2[0] < adf2[4]['5%']:
         # Both variables are stationary
-        return False
+        return False, None
     else:
         # At least one variable is non-stationary, proceed to step 2
 
@@ -71,24 +89,24 @@ def is_cointegrated(p1, p2):
         if resid_adf[0] < resid_adf[4]['5%']:
             # The variables are cointegrated
             hedge_ratio = eg_test.params[1]
-            return True
+            return True, hedge_ratio
         else:
             # The variables are not cointegrated
-            return False
+            return False, None
 
-def get_top_n_cointegrated_pairs(ssds, n):
+def get_top_n_cointegrated_pairs(ssds, n=-1):
     cointegrated_pairs = []
+    n = n if n != -1 else len(ssds)
     for pair in tqdm(ssds):
         p1, p2 = pair[0]
-        if is_cointegrated(p1, p2):
-            cointegrated_pairs.append(pair)
+        is_cointegrated, hedge_rato = get_is_cointegrated_and_hedge_ratio(p1, p2)
+        if is_cointegrated:
+            cointegrated_pairs.append((pair[0], hedge_rato))
             if len(cointegrated_pairs) == n:
                 return cointegrated_pairs
-            
-    return cointegrated_pairs
 
-# calculate_pairs_sum_of_squared_differences()
-ssds = load_pairs_sum_of_squared_differences()
-foo = get_top_n_cointegrated_pairs(ssds, 20)
-print(*foo, sep="\n")
+    return save_cointegrated_pairs(cointegrated_pairs)
 
+cointegrated_pairs = load_cointegrated_pairs()
+print(*cointegrated_pairs, sep="\n")
+print(len(cointegrated_pairs))
