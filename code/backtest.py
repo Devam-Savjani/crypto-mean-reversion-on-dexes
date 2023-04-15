c626918a0b9c3ceb1ed86e1aaa613607a5d90a9b
Devam Savjani <devamsavjani@rocketmail.com>
2023-04-15 16:36:30 +0100

    Pre-empt rebalancing

--- code/backtest.py
+++ code/backtest.py
@@ -50,6 +50,32 @@ class Backtest():
             self.account['P2'] = (position_a - (abs(position_b) / prices['P2']), position_b + abs(position_b))
             warnings.warn(f'Boosting P2[1] by {abs(position_b)}')
 
+    def check_account(self, open_or_close, buy_or_sell):
+        if self.account['P1'][0] < 0:
+            raise Exception(
+                f'Account balace goes below 0 - {open_or_close} {buy_or_sell} P1[0]')
+
+        if self.account['P1'][1] < 0:
+            raise Exception(
+                f'Account balace goes below 0 - {open_or_close} {buy_or_sell} P1[1]')
+
+        if self.account['P2'][0] < 0:
+            raise Exception(
+                f'Account balace goes below 0 - {open_or_close} {buy_or_sell} P2[0]')
+
+        if self.account['P2'][1] < 0:
+            raise Exception(
+                f'Account balace goes below 0 - {open_or_close} {buy_or_sell} P2[1]')
+
+    def swap_currencies(self, pair, should_swap_for_A, volume, prices):
+        position_a, position_b = self.account[pair]
+        if should_swap_for_A:
+            self.account[pair] = (position_a + volume, position_b - (volume * prices[pair]))
+            self.trades.append((len(self.trades), 'SWAP FOR A', pair, prices[pair], volume))
+        else:
+            self.account[pair] = (position_a - (volume / prices[pair]), position_b + volume)
+            self.trades.append((len(self.trades), 'SWAP FOR B', pair, prices[pair], volume))
+
     def backtest_pair(self, cointegrated_pair, strategy):
         print(f'cointegrated_pair: {cointegrated_pair}')
 
@@ -78,27 +104,16 @@ class Backtest():
             if signal is None:
                 continue
 
-            if 'CLOSE' in signal:
-                for buy_id, buy_position in list(signal['CLOSE']['BUY'].items()):
-                    buy_pair, bought_price, buy_volume = buy_position
-                    position_a, position_b = self.account[buy_pair]
-                    self.account[buy_pair] = (
-                        position_a - buy_volume, position_b + (prices[buy_pair] * buy_volume))
-                    open_positions['BUY'].pop(buy_id)
-                    self.rebalance_account(prices=prices)
-
-                for sell_id, sell_position in list(signal['CLOSE']['SELL'].items()):
-                    sell_pair, sold_price, sell_volume = sell_position
-                    position_a, position_b = self.account[sell_pair]
-                    self.account[sell_pair] = (position_a + (sell_volume * (
-                        (sold_price / prices[sell_pair]) - 1)), position_b - (sold_price * sell_volume))
-                    open_positions['SELL'].pop(sell_id)
-                    self.rebalance_account(prices=prices)
-
             if 'OPEN' in signal:
                 for buy_order in signal['OPEN']['BUY']:
                     buy_pair, buy_volume = buy_order
                     position_a, position_b = self.account[buy_pair]
+
+                    if position_b - (buy_volume * prices[buy_pair]) < 0:
+                        # Rebalance now
+                        self.swap_currencies(pair=buy_pair, should_swap_for_A=False, volume=abs(position_b - (buy_volume * prices[buy_pair])), prices=prices)
+                        position_a, position_b = self.account[buy_pair]
+
                     self.account[buy_pair] = (
                         position_a + buy_volume, position_b - (buy_volume * prices[buy_pair]))
 
@@ -106,8 +121,8 @@ class Backtest():
                     open_positions['BUY'][trade_id] = (
                         buy_pair, prices[buy_pair], buy_volume)
                     self.trades.append(
-                        (trade_id, buy_pair, prices[buy_pair], buy_volume))
-                    self.rebalance_account(prices=prices)
+                        (trade_id, 'BUY', buy_pair, prices[buy_pair], buy_volume))
+                    self.check_account('OPEN', 'BUY')
 
                 for sell_order in signal['OPEN']['SELL']:
                     sell_pair, sell_volume = sell_order
@@ -119,8 +134,42 @@ class Backtest():
                     open_positions['SELL'][trade_id] = (
                         sell_pair, prices[sell_pair], sell_volume)
                     self.trades.append(
-                        (trade_id, sell_pair, prices[sell_pair], sell_volume))
-                    self.rebalance_account(prices=prices)
+                        (trade_id, 'SELL', sell_pair, prices[sell_pair], sell_volume))
+                    self.check_account('OPEN', 'SELL')
+
+            if 'CLOSE' in signal:
+                for buy_id, buy_position in list(signal['CLOSE']['BUY'].items()):
+                    buy_pair, bought_price, buy_volume = buy_position
+                    position_a, position_b = self.account[buy_pair]
+
+                    if position_a - buy_volume < 0:
+                        # Rebalance now
+                        self.swap_currencies(pair=buy_pair, should_swap_for_A=True, volume=abs(position_a - buy_volume), prices=prices)
+                        position_a, position_b = self.account[buy_pair]
+
+                    self.account[buy_pair] = (
+                        position_a - buy_volume, position_b + (prices[buy_pair] * buy_volume))
+                    open_positions['BUY'].pop(buy_id)
+                    self.check_account('CLOSE', 'BUY')
+
+                for sell_id, sell_position in list(signal['CLOSE']['SELL'].items()):
+                    sell_pair, sold_price, sell_volume = sell_position
+                    position_a, position_b = self.account[sell_pair]
+
+                    if position_a + (sell_volume * ((sold_price / prices[sell_pair]) - 1)) < 0:
+                        # Rebalance Now
+                        self.swap_currencies(pair=sell_pair, should_swap_for_A=True, volume=abs(position_a + (sell_volume * ((sold_price / prices[sell_pair]) - 1))), prices=prices)
+                        position_a, position_b = self.account[sell_pair]
+
+                    if position_b - (sold_price * sell_volume) < 0:
+                        # Rebalance Now
+                        self.swap_currencies(pair=sell_pair, should_swap_for_A=False, volume=abs(position_b - (sold_price * sell_volume)), prices=prices)
+                        position_a, position_b = self.account[sell_pair]
+
+                    self.account[sell_pair] = (position_a + (sell_volume * (
+                        (sold_price / prices[sell_pair]) - 1)), position_b - (sold_price * sell_volume))
+                    open_positions['SELL'].pop(sell_id)
+                    self.check_account('CLOSE', 'SELL')
 
         print(f'Exiting Positions Without Exchanging - {self.account}')
 
