999bcf142f50ed089f637018a75df91a2eebb094
Devam Savjani <devamsavjani@rocketmail.com>
2023-04-17 01:33:34 +0100

    Change path for ini file

--- code/historical_data/database_interactions.py
+++ code/historical_data/database_interactions.py
@@ -3,7 +3,7 @@ import psycopg2
 import pandas as pd
 import sqlalchemy
 
-def config(filename='database.ini', section='postgresql'):
+def config(filename='historical_data/database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
