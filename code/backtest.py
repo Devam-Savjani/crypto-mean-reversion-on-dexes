4f267bc54210e6861be1ccf9fe3ed755e64d6e99
Devam Savjani <devamsavjani@rocketmail.com>
2023-05-15 22:02:55 +0100

    Add Aave df stuff and reordered pairs list

--- code/backtest.py
+++ code/backtest.py
@@ -8,8 +8,8 @@ from strategies.kalman_filter import Kalman_Filter_Strategy
 from tqdm import tqdm
 import sys
 sys.path.append('./historical_data')
-from calculate_cointegrated_pairs import load_cointegrated_pairs
 from database_interactions import table_to_df
+from calculate_cointegrated_pairs import load_cointegrated_pairs
 
 
 GAS_USED_BY_SWAP = 150000
@@ -31,7 +31,8 @@ class Backtest():
         return {
             'T1': 0,
             'T2': 0,
-            'WETH': account_size_in_WETH
+            'WETH': account_size_in_WETH,
+            'collateral_WETH': 0
         }
 
     def fetch_and_preprocess_data(self, cointegrated_pair, window_size_in_seconds):
@@ -44,8 +45,8 @@ class Backtest():
 
         merged = table_to_df(command=f"""
                         SELECT p1.period_start_unix as period_start_unix, p1.id as p1_id, p1.token1_price as p1_token1_price, p2.id as p2_id, p2.token1_price as p2_token1_price, p1.gas_price_wei as p1_gas_price_wei, p2.gas_price_wei as p2_gas_price_wei
-                        FROM "{cointegrated_pair[0]}" as p1 INNER JOIN "{p2}" as p2
-                        ON p1.period_start_unix = p2.period_start_unix WHERE p1.token1_price <> 0 AND p2.token1_price <> 0;
+                        FROM "{cointegrated_pair[0]}" as p1 INNER JOIN "{p2}" as p2 ON p1.period_start_unix = p2.period_start_unix
+                        WHERE p1.token1_price <> 0 AND p2.token1_price <> 0 ORDER BY p1.period_start_unix;
                         """, path_to_config='historical_data/database.ini')
 
         history_arg = merged.loc[merged['period_start_unix'] <
@@ -81,12 +82,13 @@ class Backtest():
                 f'Account balace goes below 0 - {open_or_close} {buy_or_sell} WETH')
 
     def get_account_value_in_WETH(self, prices):
-
         return (self.account['T1'] * prices['P1']) + (self.account['T2'] * prices['P2']) + self.account['WETH']
 
     def get_apy_at_timestamp(self, timestamp):
-        apy1 = self.apy1_df.iloc[(self.apy1_df['timestamp'] - timestamp).abs().argsort()[:1]]['borrow_rate'].iloc[0]
-        apy2 = self.apy2_df.iloc[(self.apy2_df['timestamp'] - timestamp).abs().argsort()[:1]]['borrow_rate'].iloc[0]
+        apy1 = self.aave1_df.iloc[(
+            self.aave1_df['timestamp'] - timestamp).abs().argsort()[:1]]['borrow_rate'].iloc[0]
+        apy2 = self.aave2_df.iloc[(
+            self.aave2_df['timestamp'] - timestamp).abs().argsort()[:1]]['borrow_rate'].iloc[0]
 
         return {
             'T1': apy1,
@@ -101,11 +103,11 @@ class Backtest():
         token2_symbol = cointegrated_pair[1].split('_')[0] if len(
             cointegrated_pair[1].split('_')) == 3 else cointegrated_pair[1].split('_')[1]
 
-        self.apy1_df = table_to_df(
-            command=f"SELECT * FROM {token1_symbol}_borrowing_rates;", path_to_config='historical_data/database.ini')
-        
-        self.apy2_df = table_to_df(
-            command=f"SELECT * FROM {token2_symbol}_borrowing_rates;", path_to_config='historical_data/database.ini')
+        self.aave1_df = table_to_df(
+            command=f"SELECT * FROM {token1_symbol}_borrowing_rates ORDER BY timestamp;", path_to_config='historical_data/database.ini')
+
+        self.aave2_df = table_to_df(
+            command=f"SELECT * FROM {token2_symbol}_borrowing_rates ORDER BY timestamp;", path_to_config='historical_data/database.ini')
 
         strategy.initialise_historical_data(
             history_p1=history_arg['p1_token1_price'], history_p2=history_arg['p2_token1_price'])
@@ -142,8 +144,15 @@ class Backtest():
             self.account[sell_token] = self.account[sell_token] + (sell_volume * (
                 (sold_price / prices[f'P{sell_token[1]}']) - 1)) - sell_volume * (hourly_yield ** number_of_hours)
             self.account['WETH'] = self.account['WETH'] - (sold_price * sell_volume) - (
-                (GAS_USED_BY_SWAP + GAS_USED_BY_LOAN) * gas_price_in_eth)
+                (GAS_USED_BY_SWAP + GAS_USED_BY_LOAN) * gas_price_in_eth) + self.account['collateral_WETH']
+            self.account['collateral_WETH'] = 0
             self.open_positions['SELL'].pop(sell_id)
+
+        weth_borrowing_rates = table_to_df(
+            command=f"SELECT * FROM weth_borrowing_rates ORDER BY timestamp;", path_to_config='historical_data/database.ini')
+
+        vtl_eth = weth_borrowing_rates['ltv'].iloc[0]
+        liquidation_threshold = weth_borrowing_rates['liquidation_threshold'].iloc[0]
 
         # for i in history_remaining.index:
         for i in tqdm(history_remaining.index):
@@ -164,10 +173,7 @@ class Backtest():
                     'gas_price_in_eth': gas_price_in_eth,
                     'timestamp': history_remaining['period_start_unix'][i],
                     'apy': apy,
-                    'vtl': {
-                        'T1': 1,
-                        'T2': 1
-                    }
+                    'vtl_eth': vtl_eth
                 }, prices)
 
             self.account_value_history.append(
@@ -209,8 +215,17 @@ class Backtest():
                 for sell_order in signal['OPEN']['SELL']:
                     sell_token, sell_volume = sell_order
                     sell_price = prices[f'P{sell_token[1]}']
-                    self.account['WETH'] = self.account['WETH'] + (sell_volume * sell_price) - (
-                        (GAS_USED_BY_SWAP + GAS_USED_BY_LOAN) * gas_price_in_eth)
+
+                    # Borrow Token from Aave
+                    self.account[sell_token] = self.account[sell_token] + sell_volume
+                    self.account['WETH'] = self.account['WETH'] - (GAS_USED_BY_LOAN * gas_price_in_eth) - ((sell_volume * sell_price) / vtl_eth)
+                    self.account['collateral_WETH'] = self.account['collateral_WETH'] + ((sell_volume * sell_price) / vtl_eth)
+
+                    # Swap borrowed tokens to WETH
+                    self.account[sell_token] = self.account[sell_token] - sell_volume
+                    self.account['WETH'] = self.account['WETH'] + \
+                        (sell_volume * sell_price) - \
+                        (GAS_USED_BY_SWAP * gas_price_in_eth)
 
                     self.open_positions['SELL'][str(next_id)] = (
                         sell_token, sell_price, sell_volume, history_remaining['period_start_unix'][i])
@@ -268,20 +283,18 @@ class Backtest():
 cointegrated_pairs = load_cointegrated_pairs(
     'historical_data/cointegrated_pairs.pickle')
 
-particular_idx = 0
-particular_idx = None
-
-num = particular_idx if particular_idx is not None else 0
-pairs = cointegrated_pairs[particular_idx:particular_idx +
-                           1] if particular_idx is not None else cointegrated_pairs
-
 ps = []
 
-for p in pairs:
+for p in cointegrated_pairs:
     if ((p[0].split('_')[1] == p[1].split('_')[1]) or (len(p[1].split('_')) == 4 and p[0].split('_')[1] == p[1].split('_')[0])) and p[0].split('_')[1] == 'WETH':
         ps.append(p)
 
-# ps = pairs
+particular_idx = 8
+particular_idx = None
+
+num = particular_idx if particular_idx is not None else 0
+pairs = ps[particular_idx:particular_idx +
+                           1] if particular_idx is not None else ps
 
 bad_pairs = []
 
@@ -290,7 +303,7 @@ window_size_in_seconds = days_to_seconds(30)
 percent_to_invest = 1.00
 initial_investment = 100
 
-for cointegrated_pair in ps:
+for cointegrated_pair in pairs:
     try:
         print(num)
         num += 1
@@ -309,19 +322,19 @@ for cointegrated_pair in ps:
             print(
                 f"\033[95mMean_Reversion_Strategy\033[0m Total returns \033[91m{return_percent}%\033[0m - trading from {datetime.fromtimestamp(backtest_mean_reversion.times[0])} to {datetime.fromtimestamp(backtest_mean_reversion.times[-1])} with {len(backtest_mean_reversion.trades)} trades")
 
-        # kalman_filter_strategy = Kalman_Filter_Strategy(number_of_sds_from_mean=number_of_sds_from_mean,
-        #                                                 window_size_in_seconds=window_size_in_seconds,
-        #                                                 percent_to_invest=percent_to_invest)
+        kalman_filter_strategy = Kalman_Filter_Strategy(number_of_sds_from_mean=number_of_sds_from_mean,
+                                                        window_size_in_seconds=window_size_in_seconds,
+                                                        percent_to_invest=percent_to_invest)
 
-        # backtest_kalman_filter = Backtest()
-        # return_percent = backtest_kalman_filter.backtest_pair(
-        #     cointegrated_pair, kalman_filter_strategy, initial_investment)
-        # if return_percent > 0:
-        #     print(
-        #         f"\033[96mKalman_Filter_Strategy\033[0m Total returns \033[92m{return_percent}%\033[0m with {len(backtest_kalman_filter.trades)} trades")
-        # else:
-        #     print(
-        #         f"\033[96mKalman_Filter_Strategy\033[0m Total returns \033[91m{return_percent}%\033[0m with {len(backtest_kalman_filter.trades)} trades")
+        backtest_kalman_filter = Backtest()
+        return_percent = backtest_kalman_filter.backtest_pair(
+            cointegrated_pair, kalman_filter_strategy, initial_investment)
+        if return_percent > 0:
+            print(
+                f"\033[96mKalman_Filter_Strategy\033[0m Total returns \033[92m{return_percent}%\033[0m with {len(backtest_kalman_filter.trades)} trades")
+        else:
+            print(
+                f"\033[96mKalman_Filter_Strategy\033[0m Total returns \033[91m{return_percent}%\033[0m with {len(backtest_kalman_filter.trades)} trades")
 
         # print(*backtest_mean_reversion.trades, sep='\n')
 
@@ -337,10 +350,9 @@ for cointegrated_pair in ps:
         # axarr[1].legend()
         # plt.tight_layout()
         # plt.show()
-    # except Exception as e:
-    #     bad_pairs.append((cointegrated_pair))
-    #     print(e)
-    #     print(backtest_mean_reversion.trades[-1])
-    #     print()
+    except Exception as e:
+        bad_pairs.append((cointegrated_pair))
+        print(e)
+        print()
     finally:
         pass
