ea02c66343f4e017b215bf53bbd9277d72c9ca71
Devam Savjani <devamsavjani@rocketmail.com>
2023-05-25 17:00:27 +0100

    Update kalman filter code

--- code/strategies/kalman_filter.py
+++ code/strategies/kalman_filter.py
@@ -1,48 +1,49 @@
+import statsmodels.api as sm
+import abstract_strategy
 import numpy as np
 from pykalman import KalmanFilter
 import sys
 sys.path.append('./strategies')
-import abstract_strategy
-import matplotlib.pyplot as plt
-import statsmodels.api as sm
 
 
 GAS_USED_BY_SWAP = 150000
 GAS_USED_BY_LOAN = 100000
 GAS_USED = (2 * GAS_USED_BY_SWAP) + GAS_USED_BY_LOAN
 
+
 class Kalman_Filter_Strategy(abstract_strategy.Abstract_Strategy):
     def __init__(self, number_of_sds_from_mean, window_size_in_seconds, percent_to_invest, gas_price_threshold, rebalance_threshold_as_percent_of_initial_investment):
-        super().__init__(number_of_sds_from_mean, window_size_in_seconds, percent_to_invest, 'Kalman', gas_price_threshold, rebalance_threshold_as_percent_of_initial_investment)
+        super().__init__(number_of_sds_from_mean, window_size_in_seconds, percent_to_invest,
+                         'Kalman', gas_price_threshold, rebalance_threshold_as_percent_of_initial_investment)
         self.hedge_ratio_history = []
 
     def initialise_historical_data(self, history_p1, history_p2):
         super().initialise_historical_data(history_p1, history_p2)
         self.initialise_kalman_filter_and_thresholds()
-    
+
     def initialise_kalman_filter_and_thresholds(self):
         p1, p2 = self.history_p1, self.history_p2
 
-        obs_mat = sm.add_constant(p1, prepend=False)[:, np.newaxis]
-        trans_cov = 1e-5 / (1 - 1e-5) * np.eye(2)
+        obs_mat = np.transpose(
+            np.vstack([p1, np.ones(len(p1))])).reshape(-1, 1, 2)
 
         model = sm.OLS(p2, sm.add_constant(p1))
         initial_state = model.fit().params[::-1]
 
-        kf = KalmanFilter(n_dim_obs=1, n_dim_state=2, 
-                  initial_state_mean=initial_state,
-                  initial_state_covariance=np.ones((2, 2)),
-                  transition_matrices=np.eye(2),
-                  observation_matrices=obs_mat,
-                  observation_covariance=0.5,
-                  transition_covariance=trans_cov)
+        kf = KalmanFilter(
+            n_dim_state=2,
+            initial_state_mean=initial_state,
+            transition_matrices=np.eye(2),
+            observation_matrices=obs_mat,
+            transition_covariance=1e-5 * np.eye(2)
+        )
 
         state_means, state_covs = kf.filter(p2)
 
         self.means_trace = [state_means[-1]]
         self.covs_trace = [state_covs[-1]]
 
-        self.hedge_ratio = state_means[-1,0]
+        self.hedge_ratio = state_means[-1, 0]
         self.kf = kf
 
         spread = self.history_p1[-self.window_size_in_hours:] - \
@@ -71,7 +72,7 @@ class Kalman_Filter_Strategy(abstract_strategy.Abstract_Strategy):
                 spread_mean + self.number_of_sds_from_mean * spread_std)
             self.lower_thresholds.append(
                 spread_mean - self.number_of_sds_from_mean * spread_std)
-        
+
     def update_hedge_ratio(self):
         p1, p2 = self.history_p1, self.history_p2
         x = p2[-1]
