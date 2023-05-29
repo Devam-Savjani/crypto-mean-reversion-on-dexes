1383cf172e37628b8a19bac5fbcb513b1f9be4d7
Devam Savjani <devamsavjani@rocketmail.com>
2023-05-29 10:55:25 +0100

    Use sql_query to get liquidity_pools

--- code/correlation_plot.py
+++ code/correlation_plot.py
@@ -4,31 +4,28 @@ from database_interactions import table_to_df
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-liquidity_pools = []
-
 tokens_supported_by_aave = ['DAI', 'EURS', 'USDC', 'USDT', 'AAVE', 'LINK', 'WBTC']
 filter_tokens = ' OR \n\t'.join([f"token0 = '{token}' OR token1 = '{token}'" for token in tokens_supported_by_aave])
 
-query = f"""
-    SELECT pool_address, token0, token1
+liquidity_pools_query = f"""
+    Select CONCAT(pool.token0, '_', pool.token1, '_', pool.pool_address) as table_name from
+    (SELECT *
     FROM liquidity_pools WHERE
     (token0='WETH' or token1='WETH') AND
     \t({filter_tokens}) AND volume_usd >= 10000000
-    ORDER BY volume_usd DESC;
+    ORDER BY volume_usd DESC) as pool
 """
 
-df = table_to_df(command=query, path_to_config='historical_data/database.ini')
+liquidity_pools = table_to_df(command=liquidity_pools_query, path_to_config='historical_data/database.ini')['table_name'].to_list()
+print(liquidity_pools)
 
-for index, row in df.iterrows():
-    liquidity_pools.append('"' + f"{row['token0']}_{row['token1']}_{row['pool_address']}" + '"')
-
-columns = ',\n\t'.join([f'p{i}.{"token1_price" if liquidity_pools[i][1:5] != "WETH" else "token0_price"} as {liquidity_pools[i][1:-1][:15]}' for i in range(1, len(liquidity_pools))])
-joins = '\n\t'.join([f'INNER JOIN {liquidity_pools[i]} as p{i} ON p1.period_start_unix = p{i}.period_start_unix' for i in range(2, len(liquidity_pools))])
-where = ' AND \n\t'.join([f'p{i}.token1_price <> 0' for i in range(1, len(liquidity_pools))])
+columns = ',\n\t'.join([f'p{i}.{"token1_price" if liquidity_pools[i][:4] != "WETH" else "token0_price"} as {liquidity_pools[i][:15]}' for i in range(0, len(liquidity_pools))])
+joins = '\n\t'.join([f'INNER JOIN "{liquidity_pools[i]}" as p{i} ON p0.period_start_unix = p{i}.period_start_unix' for i in range(1, len(liquidity_pools))])
+where = ' AND \n\t'.join([f'p{i}.token1_price <> 0' for i in range(len(liquidity_pools))])
 
 query = f"""
     SELECT \n\t{columns}
-    FROM {liquidity_pools[0]} as p1
+    FROM "{liquidity_pools[0]}" as p0
     \t{joins}
     WHERE 
     \t{where}
