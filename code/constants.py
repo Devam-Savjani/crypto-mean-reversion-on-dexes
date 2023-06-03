b5b0d4484abbff211d1472158b310ab03a059ec2
Devam Savjani <devamsavjani@rocketmail.com>
2023-06-03 11:43:41 +0100

    Use VOLUME_LOWER_LIMIT in liquidity_pools_table_of_interest

--- code/constants.py
+++ code/constants.py
@@ -1,6 +1,7 @@
 GAS_USED_BY_SWAP = 150000
 GAS_USED_BY_LOAN = 100000
 GAS_USED = (2 * GAS_USED_BY_SWAP) + GAS_USED_BY_LOAN
+VOLUME_LOWER_LIMIT = 10000000
 
 tokens_supported_by_aave = ['DAI', 'EURS', 'USDC', 'USDT', 'AAVE', 'LINK', 'WBTC']
 filter_tokens = ' OR \n\t'.join([f"token0 = '{token}' OR token1 = '{token}'" for token in tokens_supported_by_aave])
@@ -9,7 +10,7 @@ liquidity_pools_table_of_interest = f"""
     SELECT pool_address, token0, token1
     FROM liquidity_pools WHERE
     (token0='WETH' or token1='WETH') AND
-    \t({filter_tokens}) AND volume_usd >= 10000000
+    \t({filter_tokens}) AND volume_usd >= {VOLUME_LOWER_LIMIT}
     ORDER BY volume_usd DESC
 """
 
