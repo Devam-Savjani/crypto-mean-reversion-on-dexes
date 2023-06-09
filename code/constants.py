0fb2289294b8da42e984ae39e1bad15e45e10f48
Devam Savjani <devamsavjani@rocketmail.com>
2023-06-09 11:35:39 +0100

    Use select * in liquidity_pools_table_of_interest

--- code/constants.py
+++ code/constants.py
@@ -17,8 +17,7 @@ tokens_supported_by_aave = ['DAI', 'EURS', 'USDC', 'USDT', 'AAVE', 'LINK', 'WBTC
 filter_tokens = ' OR \n\t'.join([f"token0 = '{token}' OR token1 = '{token}'" for token in tokens_supported_by_aave])
 
 liquidity_pools_table_of_interest = f"""
-    SELECT pool_address, token0, token1
-    FROM liquidity_pools WHERE
+    SELECT * FROM liquidity_pools WHERE
     (token0='WETH' or token1='WETH') AND
     \t({filter_tokens}) AND volume_usd >= {VOLUME_LOWER_LIMIT}
     ORDER BY volume_usd DESC
