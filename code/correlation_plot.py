ca7b2ae7a3a0ae743788d52f5bd54c772b153718
Devam Savjani <devamsavjani@rocketmail.com>
2023-06-09 02:08:07 +0100

    Use LIQUIDITY_POOLS_OF_INTEREST_TABLENAMES_QUERY to get liquidity_pools in correlation_plot

--- code/correlation_plot.py
+++ code/correlation_plot.py
@@ -5,10 +5,10 @@ import seaborn as sn
 import matplotlib.pyplot as plt
 import numpy as np
 from check_liquidity_pool_data import get_pools_max_timestamp
-from constants import CORRELATION_LOWER_LIMIT, CORRELATION_UPPER_LIMIT
+from constants import CORRELATION_LOWER_LIMIT, CORRELATION_UPPER_LIMIT, LIQUIDITY_POOLS_OF_INTEREST_TABLENAMES_QUERY
 
 def get_correlation_matrix():
-    liquidity_pools = get_pools_max_timestamp()['table_name'].to_list()
+    liquidity_pools = table_to_df(command=LIQUIDITY_POOLS_OF_INTEREST_TABLENAMES_QUERY, path_to_config='historical_data/database.ini')['table_name'].to_list()
 
     columns = ',\n\t'.join([f'p{i}.{"token1_price" if liquidity_pools[i][:4] != "WETH" else "token0_price"} as {liquidity_pools[i]}' for i in range(0, len(liquidity_pools))])
     joins = '\n\t'.join([f'INNER JOIN "{liquidity_pools[i]}" as p{i} ON p0.period_start_unix = p{i}.period_start_unix' for i in range(1, len(liquidity_pools))])
